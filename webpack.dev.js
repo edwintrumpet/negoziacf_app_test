e5e4b9b6cc6c1c7f064f65714c816e38acfa9eee
Edwin Garcia <spark.com.co@gmail.com>
2021-07-02 06:16:22 -0500

    [setup] enable react

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+
+            },
+        ]
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+        disableHostCheck: false,
+        open: true,
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            title: 'NegoziaCF',
+            hash: false,
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'NegociaCF',
+            hash: true,
+            template: path.resolve(__dirname, 'public/index.html')
+        }),
+    ]
+}
