6d94b5e6585f294a185eb5a052512f5c182ee204
Edwin Garcia <spark.com.co@gmail.com>
2021-07-02 06:40:23 -0500

    [setup] set linter and husky to run linter pre-commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,31 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: {
-        home: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+  entry: {
+    home: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
 
-            },
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title: 'NegociaCF',
-            hash: true,
-            template: path.resolve(__dirname, 'public/index.html')
-        }),
-    ]
-}
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'NegociaCF',
+      hash: true,
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+};
